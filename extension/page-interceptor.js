c8ae102eb11a48e252a529ea6c94b5192583e6ed
Kayce Chung <chunghienkhang@gmail.com>
2026-05-29 02:40:19 +0700

    intercept XHR in addition to fetch for Klook SKU detection

--- extension/page-interceptor.js
+++ extension/page-interceptor.js
@@ -1,18 +1,31 @@
 (function () {
-  const orig = window.fetch;
+  function notifySku(url) {
+    try {
+      const skuMatch = url.match(/[?&]sku_id=(\d+)/);
+      if (!skuMatch) return;
+      const actMatch = url.match(/[?&]activity_id=(\d+)/);
+      window.postMessage({
+        type:        'KLOOK_SKU_DETECTED',
+        sku_id:      skuMatch[1],
+        activity_id: actMatch ? actMatch[1] : null,
+      }, '*');
+    } catch (_) {}
+  }
+
+  // Intercept fetch
+  const origFetch = window.fetch;
   window.fetch = function (...args) {
     try {
       const url = typeof args[0] === 'string' ? args[0] : (args[0]?.url || '');
-      const skuMatch = url.match(/[?&]sku_id=(\d+)/);
-      if (skuMatch) {
-        const actMatch = url.match(/[?&]activity_id=(\d+)/);
-        window.postMessage({
-          type:        'KLOOK_SKU_DETECTED',
-          sku_id:      skuMatch[1],
-          activity_id: actMatch ? actMatch[1] : null,
-        }, '*');
-      }
+      notifySku(url);
     } catch (_) {}
-    return orig.apply(this, args);
+    return origFetch.apply(this, args);
+  };
+
+  // Intercept XMLHttpRequest (Klook may use axios/XHR)
+  const origOpen = XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function (method, url) {
+    try { notifySku(String(url)); } catch (_) {}
+    return origOpen.apply(this, arguments);
   };
 })();
